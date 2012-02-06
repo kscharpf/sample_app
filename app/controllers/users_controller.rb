ebd86c938501d1206a08bbd31939986cb8fcea79
Kevin Scharpf <kevin.scharpf@gmail.com>
2012-02-05 19:16:37 -0800

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
-  def show
-    @user = User.find(params[:id])
-    @title = @user.name
-  end
 
   def new
     @user  = User.new
@@ -46,6 +42,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
 
@@ -56,10 +53,6 @@ class UsersController < ApplicationController
   end
 
   private
-    def authenticate
-      deny_access unless signed_in?
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)

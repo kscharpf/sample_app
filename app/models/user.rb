5333b1069930f31469554c69c2f12d891663c5cf
Kevin Scharpf <kevin.scharpf@gmail.com>
2012-02-06 20:03:40 -0800

    through listing 12.14

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -50,6 +53,14 @@ class User < ActiveRecord::Base
   def feed
     # This is preliminary.  See Chapter 12 for the full implementation.
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
   end
 
   private
